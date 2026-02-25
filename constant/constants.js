879d3d354196996ed3d47563c7e84ece76aec5c6
Rahul Chaudhary <rahuldahit@gmail.com>
2026-02-25 14:34:36 +0545

    fixed base url

--- constant/constants.js
+++ constant/constants.js
@@ -1,7 +1,6 @@
 export const ITEMS_PER_PAGES = 8;
 
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || "https://fakestoreapi.com";
+export const BASE_URL = "https://fakestoreapi.com";
 
 export const SITE_TITLE = "E-commerce Dashboard";
 
